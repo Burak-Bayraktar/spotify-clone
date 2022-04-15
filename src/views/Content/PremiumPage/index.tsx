41747a4dab389188ee572a823410bf1bf2ad86fa
Burak Bayraktar <byrktrhmx@gmail.com>
2022-04-16 02:20:27 +0300

    ADD: Premium page boilerplate added

---
+++ src/views/Content/PremiumPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.scss'
+
+const PremiumPage = () => {
+  return (
+    <div className='premium-page'>
+       <span>PREMIUM PAGE</span>
+       <span>... UNDER CONSTRUCTION ...</span>
+    </div>
+  )
+}
+
+export default PremiumPage
