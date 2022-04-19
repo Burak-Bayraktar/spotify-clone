192b11313182b240f8e629ae774e0e13ae7e5da1
Burak Bayraktar <byrktrhmx@gmail.com>
2022-04-19 23:31:13 +0300

    UPDATE: Premium page Desktop & Responsive improvements

---
+++ src/assets/svg/Checkmark.tsx
@@ -0,0 +1,21 @@
+const Checkmark = () => {
+  return (
+    <svg
+      role="img"
+      focusable="false"
+      height="24"
+      width="24"
+      viewBox="0 0 24 24"
+      aria-label=""
+      className="Svg-ytk21e-0 fJEWJR"
+    >
+      <polyline
+        points="3.32 12.86 8.9 19.4 20.99 5.26"
+        fill="none"
+        stroke="#181818"
+      ></polyline>
+    </svg>
+  );
+};
+
+export default Checkmark;
