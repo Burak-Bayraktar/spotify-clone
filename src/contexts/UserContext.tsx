ff6969e2f52a4fdb52899a793340390b617b9374
Burak Bayraktar <bayraktar@foxxum.com>
2022-11-07 03:07:12 +0300

    ADD: loading state to WebSite layout

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,8 +1,8 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { getCurrentUser } from "services/User";
-import { UserContextType } from "contexts/types/UserContext";
+import { FetchStateType, UserContextType } from "contexts/types/UserContext";
 
-const UserProps: UserContextType = {
+const UserProps: UserContextType & FetchStateType = {
   country: "",
   display_name: "",
   id: "",
@@ -10,30 +10,40 @@ const UserProps: UserContextType = {
   product: "",
   type: "",
   uri: "",
+  loading: true,
+  error: false,
 };
 
-const SpotifyUser = createContext<UserContextType>(UserProps);
+const SpotifyUser = createContext<UserContextType & FetchStateType>(UserProps);
 
 export const UserProvider: React.FC<React.ReactNode> = ({ children }) => {
   const [user, setUser] = useState<UserContextType>(UserProps);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const currentUser = getCurrentUser();
-    Promise.resolve(currentUser).then((result) => {
-      setUser({
-        country: result.country,
-        display_name: result.display_name,
-        id: result.id,
-        images: result.images,
-        product: result.product,
-        type: result.type,
-        uri: result.uri,
-      });
-    });
+    Promise.resolve(currentUser)
+      .then((result) => {
+        setUser({
+          country: result.country,
+          display_name: result.display_name,
+          id: result.id,
+          images: result.images,
+          product: result.product,
+          type: result.type,
+          uri: result.uri,
+        });
+      })
+      .catch(() => setError(true))
+      .finally(() => setTimeout(() => setLoading(false), 500));
   }, []);
 
   const values = {
     ...user,
+    loading: loading,
+    error: error,
   };
 
   return <SpotifyUser.Provider value={values}>{children}</SpotifyUser.Provider>;
