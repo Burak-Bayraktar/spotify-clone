b27f3df4f187e550e83620b2bf775a476d0f2afd
Burak Bayraktar <bayraktar@foxxum.com>
2022-11-07 03:00:09 +0300

    ADD: Spotify spinner

---
+++ src/assets/spinners/spotify-spinner.tsx
@@ -0,0 +1,17 @@
+import './style.scss';
+
+export default function SpotifySpinner() {
+  // @Stidges from Codepen: https://codepen.io/stidges/pen/VwrrGQ
+  return (
+    <div className="spinner">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  )
+}
