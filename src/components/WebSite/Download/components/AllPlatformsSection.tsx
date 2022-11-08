d30aba8b7ef95c1145176c86a7f7f63d37928492
Burak Bayraktar <byrktrhmx@gmail.com>
2022-11-08 20:02:15 +0300

    CREATE: Start Download page development

---
+++ src/components/WebSite/Download/components/AllPlatformsSection.tsx
@@ -0,0 +1,7 @@
+export const AllPlatformsSection = () => {
+  return (
+    <section className="download-page-section all-platforms">
+      AllPlatformsSection
+    </section>
+  )
+}
---
+++ src/components/WebSite/Download/components/MobileSection.tsx
@@ -0,0 +1,7 @@
+export const MobileSection = () => {
+  return (
+    <div className="download-page-section mobile-section">
+      MobileSection
+    </div>
+  )
+}
