b80f425fe4fac1c6d15fe289ca90fefe52027a8e
Burak Bayraktar <byrktrhmx@gmail.com>
2022-11-11 00:34:03 +0300

    ADD: Download section design at Download page

--- src/components/WebSite/Download/components/DownloadSection.tsx
+++ src/components/WebSite/Download/components/DownloadSection.tsx
@@ -1,3 +1,23 @@
+import { GreenCircle } from 'assets/svg/GreenCircle';
+import { GreenSuccessCheck } from 'assets/svg/GreenSuccessCheck';
+import { Laptop } from 'assets/svg/Laptop';
+import { Link } from 'react-router-dom';
+
 export const DownloadSection = () => {
-  return <section className="download-page-section download-section">DownloadSection</section>;
+  return (
+    <section className="download-page-section download-section">
+      <div className="laptop-container">
+        <Laptop />
+        <GreenCircle />
+        <GreenSuccessCheck />
+      </div>
+      <div className="section-content">
+        <div className="title">Spotify'ı İndir</div>
+        <div className="description">Cihazında milyonlarca şarkı ve podcast çal.</div>
+        <Link className="download-button" to={'#'}>
+          İndir
+        </Link>
+      </div>
+    </section>
+  );
 };
