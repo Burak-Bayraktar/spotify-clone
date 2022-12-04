23d6a14b14430f97c3241913bba7187f17f7901b
Burak Bayraktar <byrktrhmx@gmail.com>
2022-12-05 01:53:25 +0300

    UPDATE: Lazy load WebPlayerLayout components to reduce bundle size

--- src/views/Layout/WebPlayer/index.tsx
+++ src/views/Layout/WebPlayer/index.tsx
@@ -1,16 +1,18 @@
 import SpotifySpinner from 'assets/spinners/spotify-spinner';
 import { useUser } from 'contexts/UserContext';
 import { useUserOs } from 'hooks/useUserOs';
+import { lazy, Suspense } from 'react';
 import { EOSTypes } from 'types/WebSite/DownloadPage';
-import WebPlayerHeader from 'views/Layout/WebPlayer/Header';
-import MainSection from 'views/Layout/WebPlayer/Main';
-import WebPlayerSidebar from 'views/Layout/WebPlayer/Sidebar';
-import NowPlayingBar from './NowPlayingBar';
 import './style.scss';
 
 const WebPlayerLayout = () => {
   const { loading } = useUser();
   const { userDeviceType } = useUserOs();
+
+  const WebPlayerHeader = lazy(() => import('views/Layout/WebPlayer/Header'));
+  const WebPlayerSidebar = lazy(() => import('views/Layout/WebPlayer/Sidebar'));
+  const MainSection = lazy(() => import('views/Layout/WebPlayer/Main'));
+  const NowPlayingBar = lazy(() => import('views/Layout/WebPlayer/NowPlayingBar'));
 
   if (loading) {
     return <SpotifySpinner />;
@@ -18,12 +20,14 @@ const WebPlayerLayout = () => {
 
   const getDesktopVersion = () => {
     return (
-      <div className="web-player-container">
-        <WebPlayerHeader />
-        <WebPlayerSidebar />
-        <MainSection />
-        <NowPlayingBar />
-      </div>
+      <Suspense fallback={<SpotifySpinner />}>
+        <div className="web-player-container">
+          <WebPlayerHeader />
+          <WebPlayerSidebar />
+          <MainSection />
+          <NowPlayingBar />
+        </div>
+      </Suspense>
     );
   };
 
