8c852799c24dff4df09d9edf5ec7a89da18cd4bc
Burak Bayraktar <byrktrhmx@gmail.com>
2022-12-05 01:43:25 +0300

    ADD: split web player layout as desktop and mobile version

--- src/views/Layout/WebPlayer/index.tsx
+++ src/views/Layout/WebPlayer/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useUserOs } from 'hooks/useUserOs';
+import { EOSTypes } from 'types/WebSite/DownloadPage';
 import WebPlayerHeader from 'views/Layout/WebPlayer/Header';
 import MainSection from 'views/Layout/WebPlayer/Main';
 import WebPlayerSidebar from 'views/Layout/WebPlayer/Sidebar';
@@ -6,14 +7,24 @@ import NowPlayingBar from './NowPlayingBar';
 import './style.scss';
 
 const WebPlayerLayout = () => {
-  return (
-    <div className="web-player-container">
-      <WebPlayerHeader />
-      <WebPlayerSidebar />
-      <MainSection />
-      <NowPlayingBar />
-    </div>
-  );
+  const { userDeviceType } = useUserOs();
+
+  const getDesktopVersion = () => {
+    return (
+      <div className="web-player-container">
+        <WebPlayerHeader />
+        <WebPlayerSidebar />
+        <MainSection />
+        <NowPlayingBar />
+      </div>
+    );
+  };
+
+  const getMobileVersion = () => {
+    return <div className="flex justify-center mt-10 uppercase font-semibold">Mobile version is under development</div>;
+  };
+
+  return userDeviceType?.os === EOSTypes.Windows ? getDesktopVersion() : getMobileVersion();
 };
 
 export default WebPlayerLayout;
