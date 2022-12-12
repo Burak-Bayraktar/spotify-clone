6a2659f0bc4a1f8743253df99c39f1fc7adecb43
Burak Bayraktar <byrktrhmx@gmail.com>
2022-12-12 23:47:27 +0300

    ADD: create InstallApp component to add Player view Sidebar

---
+++ src/components/WebPlayer/Sidebar/InstallApp/index.tsx
@@ -0,0 +1,13 @@
+import InstallAppSvg from 'assets/svg/InstallApp';
+import './style.scss';
+
+const InstallApp = () => {
+  return (
+    <div className="install-app-container">
+      <InstallAppSvg />
+      Install App
+    </div>
+  );
+};
+
+export default InstallApp;
