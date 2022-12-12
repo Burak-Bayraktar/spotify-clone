cf73bc762ecdb755d54a7b90301341dc13f7ed99
Burak Bayraktar <byrktrhmx@gmail.com>
2022-12-12 23:46:41 +0300

    ADD: add new svg files for Sidebar

---
+++ src/assets/svg/Player_NavigationSection/YourEpisodes/YourEpisodes_Regular.tsx
@@ -0,0 +1,9 @@
+import { SVGProps } from "react";
+
+const YourEpisodesSvg = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg {...props} role="img" height="12" width="12" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" className="Svg-sc-ytk21e-0 uPxdw"><path d="M3.75 0A1.75 1.75 0 002 1.75v12.952c0 1.051 1.22 1.633 2.037.972l3.962-3.208 3.943 3.204c.817.663 2.038.082 2.038-.97V1.75A1.75 1.75 0 0012.23 0H3.75z"></path></svg>
+  )
+}
+
+export default YourEpisodesSvg;
