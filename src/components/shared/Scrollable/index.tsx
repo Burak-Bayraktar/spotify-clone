4fc6383c95d56ce77a11e90c3a797e1756fb5244
Burak Bayraktar <byrktrhmx@gmail.com>
2022-12-26 02:02:35 +0300

    ADD: Calculate custom scroll bar position onScroll event

--- src/components/shared/Scrollable/index.tsx
+++ src/components/shared/Scrollable/index.tsx
@@ -9,25 +9,30 @@ type ScrollableProps = {
 
 const Scrollable = (props: ScrollableProps) => {
   const barRef = useRef<HTMLDivElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
   const [top, setTop] = useState<number>(0);
 
   const handleThumbPosition = useCallback(() => {
-    const { scrollTop: contentTop } = props.elementRef;
-    setTop(contentTop);
-  }, [props.elementRef, containerRef.current]);
+    const { scrollTop, offsetHeight, scrollHeight } = props.elementRef;
+
+    const maxScrollable = scrollHeight - offsetHeight;
+    const targetTop = offsetHeight - Number(getComputedStyle(barRef.current!).height.slice(0, -2));
+    const ratio = targetTop / maxScrollable;
+
+    const scrollBarCurrentTop = scrollTop * ratio;
+    setTop(scrollBarCurrentTop);
+  }, [props.elementRef, barRef.current]);
 
   useEffect(() => {
     props.elementRef?.addEventListener('scroll', (e) => {
       handleThumbPosition();
     });
-  }, [props.elementRef, containerRef.current]);
+  }, [props.elementRef, barRef.current]);
 
   return (
     <div className="scrollable" style={{ height: props.height }}>
       {props.children}
-      <div ref={containerRef} className="scroll-container" style={{ top, height: '100%' }}>
-        <div ref={barRef} className="scroll-bar" />
+      <div className="scroll-container">
+        <div ref={barRef} className="scroll-bar" style={{ top }} />
       </div>
     </div>
   );
