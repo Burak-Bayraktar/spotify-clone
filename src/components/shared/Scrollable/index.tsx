545f66018b863f8c391897773dd67a0258f9fb1c
Burak Bayraktar <byrktrhmx@gmail.com>
2023-01-07 23:59:50 +0300

    ADD: calculate target scrollTop and custom scroll bar thumb position on mouse move on Custom Scroll Bar

--- src/components/shared/Scrollable/index.tsx
+++ src/components/shared/Scrollable/index.tsx
@@ -10,29 +10,105 @@ type ScrollableProps = {
 const Scrollable = (props: ScrollableProps) => {
   const barRef = useRef<HTMLDivElement>(null);
   const [top, setTop] = useState<number>(0);
+  const [isBarActive, setIsBarActive] = useState<boolean>(false);
 
-  const handleThumbPosition = useCallback(() => {
-    const { scrollTop, offsetHeight, scrollHeight } = props.elementRef;
+  let mouseYCoordRelativeToThumb = 0;
+  let mouseYCoordRelativeToThumbRest = 0;
 
-    const maxScrollable = scrollHeight - offsetHeight;
-    const targetTop = offsetHeight - Number(getComputedStyle(barRef.current!).height.slice(0, -2));
-    const ratio = targetTop / maxScrollable;
+  useEffect(() => {
+    addScrollBarListeners();
+
+    return () => {
+      props.elementRef?.removeEventListener('scroll', calcThumbAndTargetPositionOnScroll);
+      barRef.current?.removeEventListener('mousedown', subscribeMouseToCalcThumbAndTargetPosition);
+      document.removeEventListener('mouseup', unsubscribeMouseToCalcThumbAndTargetPosition);
+    };
+  }, [props.elementRef, barRef.current]);
+
+  const calcThumbAndTargetPositionOnScroll = useCallback(() => {
+    if (!props.elementRef) return;
+
+    const { scrollTop } = props.elementRef;
+    const { ratio } = calcMeasurementValues();
 
     const scrollBarCurrentTop = scrollTop * ratio;
     setTop(scrollBarCurrentTop);
   }, [props.elementRef, barRef.current]);
 
-  useEffect(() => {
-    props.elementRef?.addEventListener('scroll', (e) => {
-      handleThumbPosition();
-    });
-  }, [props.elementRef, barRef.current]);
+  const calcThumbAndTargetPositionOnMouseMove = useCallback(
+    (e: MouseEvent) => {
+      if (!props.elementRef || !barRef.current) return;
+
+      const thumbViewportTop = barRef.current.getBoundingClientRect().top;
+      const thumbViewportBottom = barRef.current.getBoundingClientRect().bottom;
+
+      if (e.clientY < mouseYCoordRelativeToThumb + thumbViewportTop && e.movementY > 0) return;
+      if (e.clientY > thumbViewportBottom - mouseYCoordRelativeToThumbRest && e.movementY < 0) return;
+
+      const { ratio, targetTop } = calcMeasurementValues();
+
+      setTop((prev) => {
+        if (prev + e.movementY < 0) {
+          props.elementRef.scrollTop = prev / ratio;
+
+          return prev;
+        }
+
+        if (prev + e.movementY > targetTop) {
+          props.elementRef.scrollTop = targetTop / ratio;
+          return targetTop;
+        }
+
+        props.elementRef.scrollTop = (prev + e.movementY) / ratio;
+        return prev + e.movementY;
+      });
+    },
+    [props.elementRef, barRef.current]
+  );
+
+  const addScrollBarListeners = () => {
+    props.elementRef?.addEventListener('scroll', calcThumbAndTargetPositionOnScroll);
+    barRef.current?.addEventListener('mousedown', subscribeMouseToCalcThumbAndTargetPosition);
+    document.addEventListener('mouseup', unsubscribeMouseToCalcThumbAndTargetPosition);
+  };
+
+  const subscribeMouseToCalcThumbAndTargetPosition = (e: MouseEvent) => {
+    setIsBarActive(true);
+
+    const rect = (e.target as HTMLDivElement).getBoundingClientRect();
+    const height = getComputedStyle(e.target as HTMLDivElement).height;
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const rest = Number(height.slice(0, -2)) - y;
+
+    mouseYCoordRelativeToThumb = y;
+    mouseYCoordRelativeToThumbRest = rest;
+
+    document?.addEventListener('mousemove', calcThumbAndTargetPositionOnMouseMove);
+  };
+
+  const unsubscribeMouseToCalcThumbAndTargetPosition = () => {
+    setIsBarActive(false);
+    document?.removeEventListener('mousemove', calcThumbAndTargetPositionOnMouseMove);
+  };
+
+  const calcMeasurementValues = () => {
+    const { offsetHeight, scrollHeight } = props.elementRef;
+
+    const maxScrollable = scrollHeight - offsetHeight;
+    const targetTop = offsetHeight - Number(getComputedStyle(barRef.current!).height.slice(0, -2));
+    const ratio = targetTop / maxScrollable;
+
+    return { ratio, targetTop };
+  };
 
   return (
     <div className="scrollable" style={{ height: props.height }}>
       {props.children}
       <div className="scroll-container">
-        <div ref={barRef} className="scroll-bar" style={{ top }} />
+        <div ref={barRef} className={`scroll-bar ${isBarActive ? '-active' : ''}`} style={{ top }} />
       </div>
     </div>
   );
