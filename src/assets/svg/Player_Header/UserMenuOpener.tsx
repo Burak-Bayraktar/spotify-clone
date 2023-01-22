ca266d4f1d446172b6d39cd0959c16aa74dd1d89
Burak Bayraktar <byrktrhmx@gmail.com>
2023-01-22 22:04:43 +0300

    ADD: Create svg files that will be used on Player Header component

---
+++ src/assets/svg/Player_Header/UserMenuOpener.tsx
@@ -0,0 +1,7 @@
+const UserMenuOpener = () => {
+  return (
+    <svg role="img" height="16" width="16" fill="white" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon"><path d="M14 6l-6 6-6-6h12z"></path></svg>
+  )
+}
+
+export default UserMenuOpener;
