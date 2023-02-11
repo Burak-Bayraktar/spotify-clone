9713079382af6014f6bdd0304cb2bae4207c096f
Burak Bayraktar <byrktrhmx@gmail.com>
2023-02-11 15:40:01 +0300

    UPDATE: Refactor useGetActiveSubComponent hook, Add new routes there to render sub components correctly

--- src/views/Layout/WebPlayer/Header/hooks/useGetActiveSubComponent.tsx
+++ src/views/Layout/WebPlayer/Header/hooks/useGetActiveSubComponent.tsx
@@ -1,28 +1,26 @@
 import { lazy, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
-import { PlayerViewNames, PlayerViewPaths } from 'views/routes';
+import { AllPlayerViewNames, AllPlayerViewPaths } from 'types/WebPlayer/routeTypes';
+
+type RouteInformation = { path: AllPlayerViewPaths; element: React.LazyExoticComponent<() => JSX.Element> };
 
 const useGetActiveSubComponent = () => {
   const { pathname } = useLocation();
   const SearchBar = useMemo(() => lazy(() => import('components/WebPlayer/Header/sub-components/SearchBar/index')), []);
-  const PlaylistFilters = useMemo(
-    () => lazy(() => import('components/WebPlayer/Header/sub-components/PlaylistFilters/index')),
-    []
-  );
+  const PlaylistFilters = useMemo(() => lazy(() => import('components/WebPlayer/Header/sub-components/PlaylistFilters/index')), []);
 
-  const headerSubComponents: Partial<
-    Record<PlayerViewNames, { path: PlayerViewPaths; element: React.LazyExoticComponent<() => JSX.Element> }>
-  > = {
+  const headerSubComponents: Partial<Record<AllPlayerViewNames, RouteInformation>> = {
     search: { path: 'search', element: SearchBar },
-    playlist: { path: 'collection', element: PlaylistFilters },
+    collection_albums: { path: 'collection/albums', element: PlaylistFilters },
+    collection_artists: { path: 'collection/artists', element: PlaylistFilters },
+    collection_playlists: { path: 'collection/playlists', element: PlaylistFilters },
+    collection_podcasts: { path: 'collection/podcasts', element: PlaylistFilters },
   };
 
   const activeSubHeaderComponent = useMemo(() => {
-    const base: PlayerViewPaths = '/player';
+    const base: AllPlayerViewPaths = '/player';
 
-    const headerSubcomponent:
-      | { path: PlayerViewPaths; element: React.LazyExoticComponent<() => JSX.Element> }
-      | undefined = Object.values(headerSubComponents).find((item) => {
+    const headerSubcomponent: RouteInformation | undefined = Object.values(headerSubComponents).find((item) => {
       return pathname.startsWith(`${base}/${item.path}`);
     });
 
